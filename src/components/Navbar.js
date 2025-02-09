4a1960da3b14d7e3168706d98dd0e651fdf5cc44
alantao5056 <alantaolr@outlook.com>
2025-02-08 20:15:22 -0500

    organize icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ export default function Navbar() {
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            <img className="logo" src="../../images/progclublogo.png"/>
+            <img className="logo" src="images/icons/progclublogo.png"/>
           </Link>
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -61,17 +61,17 @@ export default function Navbar() {
               }
               
               <li className="nav-item d-flex align-items-center ms-3">
-                <Link to="https://www.instagram.com/nnhsprogramming/" target="_blank"><img className="social-logo" src="../../images/instagram.png"/></Link>
+                <Link to="https://www.instagram.com/nnhsprogramming/" target="_blank"><img className="social-logo" src="images/icons/instagram.png"/></Link>
               </li>
               <li className="nav-item d-flex align-items-center ms-2">
-                <Link to="https://www.facebook.com/groups/293459344434857/" target="_blank"><img className="social-logo" src="../../images/facebook.png"/></Link>
+                <Link to="https://www.facebook.com/groups/293459344434857/" target="_blank"><img className="social-logo" src="images/icons/facebook.png"/></Link>
               </li>
               <li className="nav-item d-flex align-items-center ms-2 me-2">
                 {!darkMode && (
-                  <Link to="https://github.com/NNHS-Programming-Club/website" target="_blank"><img className="social-logo" src="../../images/github-dark.png"/></Link>
+                  <Link to="https://github.com/NNHS-Programming-Club/website" target="_blank"><img className="social-logo" src="images/icons/github-dark.png"/></Link>
                 )}
                 {darkMode && (
-                  <Link to="https://github.com/NNHS-Programming-Club/website" target="_blank"><img className="social-logo" src="../../images/github-light.png"/></Link>
+                  <Link to="https://github.com/NNHS-Programming-Club/website" target="_blank"><img className="social-logo" src="images/icons/github-light.png"/></Link>
                 )}
               </li>
             </ul>
