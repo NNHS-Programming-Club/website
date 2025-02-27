e54e80d43929feff4e0e9e6ea71ab03112d29c8b
alantao5056 <alantaolr@outlook.com>
2025-02-26 20:14:22 -0500

    fixed projects but still needs work

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,56 +2,28 @@ import React from 'react'
 import './Projects.css'
 
 export default function Projects() {
+  const projects = [
+    {name: "Yet Another Packer", author: "Ewan McPhail", link: "https://undisassemble.github.io/yap.html", description: "A free and open source protector for 64-bit Windows PEs."},
+    {name: "InjectDumper", author: "Ewan McPhail", link: "https://github.com/undisassemble/InjectDumper", description: "Detects and dumps injected code, and can reconstruct some manually mapped PEs."},
+    {name: "PEFixup", author: "Ewan McPhail", link: "https://github.com/undisassemble/PEFixup", description: "Tool to dump running processes and search for OEP signatures."},
+    {name: "Orbit App", author: "Betina Kreiman", link: "https://apps.apple.com/us/app/orbit-a-space-game/id1353390547", description: "A neat scroller app game to play when className gets boring. We used swift/xcode and even made the music with garage band. It's now relased in the app store!"},
+    {name: "Parking App", author: "Betina Kreiman", link: "https://apps.apple.com/us/app/north-parking/id1439643706", description: "The Newton North Parking Availability App is a great way for students to find parking around the school. Most kids with their license are aware of the chaos and disorder there is around North when trying to find parking. The app is a great way to locate parking easily and quickly."},
+    {name: "Educational Excellence Campaign", author: "Betina Kreiman", link: "https://betinakreiman.github.io/edExCampaign/", description: "This website is for the Educational Excellence Campaign that advocates for the raising of money for physics materials at the high school of Newton North. "}
+  ];
+
   return (
     <div>
       <h1>Projects</h1>
-      <div className="d-flex p-4">
-        {/* Note: These projects were taken from the old NNHS programming club website */}
-        <div className="card project m-2">
-          <div className="card-body">
-            <h4><a className="card-title" href="https://apps.apple.com/us/app/orbit-a-space-game/id1353390547">Orbit App</a></h4>
-            <p className="card-text mt-2">A neat scroller app game to play when className gets boring. We used swift/xcode and even made the music with garage band. It's now relased in the app store!</p>
+      <div className="d-flex flex-wrap justify-content-center p-4">
+        {projects.map((project) => (
+          <div className="card project m-2" onClick={() => {window.open(project.link)}}>
+            <div className="card-body" href={project.link} target="_blank">
+              <h4 className="card-title">{project.name}</h4>
+              <h6 class="card-subtitle mb-3 text-muted">{project.author}</h6>
+              <p className="card-text mt-2">{project.description}</p>
+            </div>
           </div>
-        </div>
-        <div className="card project m-2">
-          <div className="card-body">
-            <h4><a className="card-title" href="https://apps.apple.com/us/app/north-parking/id1439643706">Parking App</a></h4>
-            <p>The Newton North Parking Availability App is a great way for students to find parking around the school. Most kids with their license are aware of the chaos and disorder there is around North when trying to find parking. The app is a great way to locate parking easily and quickly.</p>
-          </div>
-        </div>
-        <div className="card project m-2">
-          <div className="card-body">
-            <h4><a className="card-title" href="https://betinakreiman.github.io/edExCampaign/">Educational Excellence Campaign</a></h4>
-            <p>This website is for the Educational Excellence Campaign that advocates for the raising of money for physics materials at the high school of Newton North. Using bootstrap, w3schools, GSAP, and many other prominent libraries of greatness, this website is both visually and mentally pleasing. We incentivize you to constantly reload the page and admire the beautiful animation. Have a wonderful day and do not forget to donate to the Educational Excellence Campaign!</p>
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        {/* Note: These projects were taken from the old NNHS programming club website */}
-        <div className="card mb-3">
-          <a className="name mt-1 ms-2" href="https://apps.apple.com/us/app/orbit-a-space-game/id1353390547">Orbit App</a>
-          <p className="mt-1 ms-2 mb-2">A neat scroller app game to play when className gets boring. We used swift/xcode and even made the music with garage band. It's now relased in the app store!</p>
-        </div>
-        <div className="card mb-3">
-          <a className="name mt-1 ms-2" href="https://apps.apple.com/us/app/north-parking/id1439643706">Parking App</a>
-          <p className="mt-1 ms-2 mb-2">The Newton North Parking Availability App is a great way for students to find parking around the school. Most kids with their license are aware of the chaos and disorder there is around North when trying to find parking. The app is a great way to locate parking easily and quickly.</p>
-        </div>
-        <div className="card mb-3">
-          <a className="name mt-1 ms-2" href="https://betinakreiman.github.io/edExCampaign/">Educational Excellence Campaign</a>
-          <p className="mt-1 ms-2 mb-2">This website is for the Educational Excellence Campaign that advocates for the raising of money for physics materials at the high school of Newton North. Using bootstrap, w3schools, GSAP, and many other prominent libraries of greatness, this website is both visually and mentally pleasing. We incentivize you to constantly reload the page and admire the beautiful animation. Have a wonderful day and do not forget to donate to the Educational Excellence Campaign!</p>
-        </div>
-        <div className="card mb-3">
-          <a className="name mt-1 ms-2" href="https://undisassemble.github.io/yap.html">Yet Another Packer</a>
-          <p className="mt-1 ms-2 mb-2">A free and open source protector for 64-bit Windows PEs.</p>
-        </div>
-        <div className="card mb-3">
-          <a className="name mt-1 ms-2" href="https://github.com/undisassemble/InjectDumper">InjectDumper</a>
-          <p className="mt-1 ms-2 mb-2">Detects and dumps injected code, and can reconstruct some manually mapped PEs.</p>
-        </div>
-        <div className="card mb-3">
-          <a className="name mt-1 ms-2" href="https://github.com/undisassemble/PEFixup">PEFixup</a>
-          <p className="mt-1 ms-2 mb-2">Tool to dump running processes and search for OEP signatures.</p>
-        </div>
+        ))}
       </div>
     </div>
   )
