5186362d58e66d981c371a5cd05e3a34853caf77
alantao5056 <alantaolr@outlook.com>
2025-05-15 21:25:53 -0400

    fixed merge bugs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,9 +12,9 @@ export default function Home() {
   useEffect(() => {
     cubesRef.current = cubes;
   }, [cubes]);
-  const [numbers] = useState(Array(8).fill(null).map((_, i) => i));
+  const [numbers] = useState(Array(10).fill(null).map((_, i) => i));
 
-  // Cube Animation
+  //cube animation
 
   const setOneCube = (i, newvars) => {
     setCubes(prev => {
@@ -45,7 +45,7 @@ export default function Home() {
       }
     }
 
-    if (i + direction < cubes.length && i + direction >= 0 && len - 1 > 0) {
+    if (i + direction !== cubes.length && i + direction !== 0 && len - 1 > 0) {
       setTimeout(clean, 50, i + direction, direction, len - 1)
     }
   }
@@ -105,76 +105,83 @@ export default function Home() {
   }, []);
 
   return (
-    <div id="first">
-      <div className="introtext">
-
-
-      {/* Title and Subtitle */}
-        <div className="intro">
-          <div id="credit">
-            <p className="hosted">Hosted by Newton North <img src="images/tiger2.png" id="tigerimg"/></p>
-          </div>
-
-          <h1 className="title" id="title">
-            <span id="firsttitle">{prgmtitle}</span>
-            {'\n'}
-            <span id="secondtitle">{clubtitle}</span>
-          </h1>
-          <p id="minititle">Teaching ambitious students to code in various languages.</p>
-        </div>
-
-
-        {/* Code Info Bbox */}
-        <div id="infoborder">
-          <div id="info">
-
-            <div id="numbers">
-              {numbers.map((n, i) => (
-                <div className='num' key={i}>
-                  <p className="numtext">{n}</p>
-                </div>
-              ))}
+    <div className="home">
+      <div id="first">
+        <div className="introtext">
+          <div className="intro">
+            <div id="credit">
+              <p className="hosted">Hosted By</p>
+              <img src="images/tiger2.png" id="tigerimg" alt="" />
+              <p className="hosted">Newton North</p>
             </div>
-
-            <div id="divider"></div>
-            <div id="lines">
-
-              <div id="line">
-                <p id="code">
-                  <span className="boldcode">const</span> info = {'{'}
-                </p>
+            <h1 className="title" id="title">
+              <span id="firsttitle">{prgmtitle}</span>
+              {' '}
+              <span id="secondtitle">{clubtitle}</span>
+            </h1>
+            <p id="minititle">Teaching ambitious students to code in various languages.</p>
+          </div>
+          <div id="infoborder">
+            <div id="info">
+              <div id="numbers">
+                {numbers.map((n, i) => (
+                  <div className='num' key={i}>
+                    <p className="numtext">{n}</p>
+                  </div>
+                ))}
               </div>
+              <div id="divider"></div>
+              <div id="lines">
 
-              <div id="line"></div>
-
-              <div id="line" className="indent">
-                <p id="code" >
-                  <span style={{ fontWeight: 'bold' }}>roomNumber</span>: <span style={{ color: 'CornflowerBlue' }}>471</span>,
-                </p>
+                <div id="line">
+                  <p id="code">
+                    <span className="boldcode">
+                      function
+                    </span>
+                    {" "}
+                    info() {"{"}
+                  </p>
+                </div>
+                <div id="line"><p id="code"></p></div>
+                <div id="line" className="i1">
+                  <p id="code" >
+                    <span className="boldcode">var</span>
+                    {" "}
+                    RoomNumber = 471
+                  </p>
+                </div>
+                <div id="line" className="i1">
+                  <p id="code" >
+                    <span className="boldcode">
+                      var
+                    </span>
+                    {" "}
+                    Time = 4:00pm - 5:00pm;
+                  </p>
+                </div>
+                <div id="line" className="i1">
+                  <p id="code" >
+                    <span className="boldcode">
+                      var
+                    </span>
+                    {" "}
+                    Day = Monday;
+                  </p>
+                </div>
+                <div id="line"><p id="code"></p></div>
+                <div id="line" className="i1">
+                  <p id="code">
+                    <span className="comment">#Zero experience is required.</span>
+                  </p>
+                </div>
+                <div id="line"><p id="code"></p></div>
+                <div id="line" className="i1">
+                  <p id="code">
+                    <span className="boldcode">return</span><span>;</span>
+                  </p>
+                </div>
+                <div id="line"><p id="code">{"}"}</p></div>
               </div>
-
-              <div id="line" className="indent">
-                <p id="code" >
-                  <span style={{ fontWeight: 'bold' }}>time</span>: <span style={{ color: "orange"}}>"4:00pm - 5:00pm"</span>,
-                </p>
-              </div>
-
-              <div id="line" className="indent">
-                <p id="code" >
-                  <span style={{ fontWeight: 'bold' }}>day</span>: <span style={{ color: "orange"}}>"Monday"</span>
-                </p>
-              </div>
-
-
-              <div id="line" className="indent">
-                <p id="code">
-                  <span className="comment">// Zero experience required!</span>
-                </p>
-              </div>
-
-              <div id="line"><p id="code"></p></div>
-
-              <div id="line"><p id="code">{"};"}</p></div>
             </div>
           </div>
         </div>
